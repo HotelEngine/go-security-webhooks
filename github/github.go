5464ad47a580667979b82be9ddbecb1202c1f6cd
Jon Rumion <jonathan.rumion@hotelengine.com>
2025-01-29 10:33:38 -0700

    Add Branch Protection Events

--- github/github.go
+++ github/github.go
@@ -75,6 +75,7 @@ const (
 	WorkflowRunEvent                         Event = "workflow_run"
 	GitHubAppAuthorizationEvent              Event = "github_app_authorization"
 	CodeScanningAlertEvent                   Event = "code_scanning_alert"
+	BranchProtectionRuleEvent                Event = "branch_protection_rule"
 )
 
 // EventSubtype defines a GitHub Hook Event subtype
@@ -356,6 +357,10 @@ func (hook Webhook) Parse(r *http.Request, events ...Event) (interface{}, error)
 		return pl, err
 	case CodeScanningAlertEvent:
 		var pl CodeScanningAlertPayload
+		err = json.Unmarshal([]byte(payload), &pl)
+		return pl, err
+	case BranchProtectionRuleEvent:
+		var pl BranchProtectionRulePayload
 		err = json.Unmarshal([]byte(payload), &pl)
 		return pl, err
 	default:
